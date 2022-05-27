084ea4cfdd61db89d70d2319525d3595b1cf4e43
Mitchell Revalski <56605543+mrevalski@users.noreply.github.com>
2022-05-27 08:58:40 -0400

    commented out import fitandplot
    The 'fitandplot()' function defined in fitting.py is no longer used in the fitting procedure, and has been commented out ahead of removal in the next commit.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 import copy as cp
-import scipy.signal as si 
+import scipy.signal as si
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
 import os
 import astropy.io.ascii as asciitable
-import astropy.io.fits as fits 
-#import time  
+import astropy.io.fits as fits
+#import time
 import mpfit
 #import multiprocessing as mp
 import math
@@ -15,22 +15,22 @@ from scipy import interpolate
 from scipy import integrate
 
 from trim_spec import trim_spec
-from utilities import gaussian 
+from utilities import gaussian
 from utilities import is_number
 from utilities import read_config
-#from specmodel import emissionline_model_spline 
-#from specmodel import model_resid_spline 
-from find_cwt import find_cwt  
+#from specmodel import emissionline_model_spline
+#from specmodel import model_resid_spline
+from find_cwt import find_cwt
 from find_cwt  import loop_field_cwt
-from find_cwt import test_obj_cwt 
+from find_cwt import test_obj_cwt
 from fitting import emissionline_model
-from fitting import model_resid 
+from fitting import model_resid
 from fitting import fit_obj
-from fitting import fitandplot
-from guis import * 
-from measure_z_interactive import * 
-import pickle 
-from gather_secure_sample import * 
+#from fitting import fitandplot # MDR 2022/05/26 - Defined in fitting.py but not used so commented out.
+from guis import *
+from measure_z_interactive import *
+import pickle
+from gather_secure_sample import *
 from measure_stack import *
 from dustfromstack import *
 from mle_stack import *
@@ -38,8 +38,7 @@ from mle_stack import *
 
 
 try:
-    from stacking import * 
+    from stacking import *
 except ImportError:
     pass
 #    print 'No stacking module. It is not needed for line finding. Skipping'
-
